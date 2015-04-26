2c4efa867f371ba07e461922a32e1eebd590e360
Mika Attila <radiantstatue@gmail.com>
2015-04-26 15:01:29 +0200

    Handle command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, World!\n");
+    let mut args = std::env::args_os().skip(1);
+    let nx_file_path = args.next().expect("Need path to nx file as first argument.");
+    let mount_path = args.next().expect("Need mount path as second argument.");
 }
