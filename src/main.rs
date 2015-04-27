e8396f802b5acfb6061f51504e4e18c13d55bfce
Mika Attila <radiantstatue@gmail.com>
2015-04-27 18:47:29 +0200

    Directory listing working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate nx;
 extern crate fuse;
+extern crate libc;
 
 mod nx_filesystem;
 
@@ -9,7 +10,8 @@ fn main() {
     let mut args = std::env::args_os().skip(1);
     let nx_file_path = args.next().expect("Need path to nx file as first argument.");
     let mount_path = args.next().expect("Need mount path as second argument.");
-    let fs = NxFilesystem::open_nx_file(nx_file_path.as_ref())
-                 .unwrap_or_else(|e| panic!("Can't open nx file: {}", e));
+    let nx_file = nx::File::open(nx_file_path.as_ref())
+                      .unwrap_or_else(|e| panic!("Can't open nx file: {}", e));
+    let fs = NxFilesystem::new_with_nx_file(&nx_file);
     fuse::mount(fs, &mount_path, &[]);
 }
--- src/nx_filesystem.rs
+++ src/nx_filesystem.rs
@@ -1,19 +1,105 @@
-use std::path::Path;
-
+use fuse::{
+    Filesystem,
+    Request,
+    ReplyDirectory,
+    FileType,
+    FUSE_ROOT_ID
+};
+use libc::ENOENT;
 use nx;
-use fuse::Filesystem;
 
-pub struct NxFilesystem {
-    nx_file: nx::File
+pub struct NxFilesystem<'a> {
+    nx_file: &'a nx::File,
+    inode_node_pairs: InodeNodePairVec<'a>,
+    /// Used to assign a new unique inode to nx nodes that don't have one yet
+    inode_counter: u64
 }
 
-impl NxFilesystem {
-    pub fn open_nx_file(path: &Path) -> Result<NxFilesystem, nx::Error> {
-        Ok(NxFilesystem {
-            nx_file: try!(nx::File::open(path))
-        })
+struct InodeNodePair<'a> {
+    inode: u64,
+    node: nx::Node<'a>
+}
+
+struct InodeNodePairVec<'a> {
+    vec: Vec<InodeNodePair<'a>>
+}
+
+impl<'a> InodeNodePairVec<'a> {
+    fn new() -> InodeNodePairVec<'a> {
+        InodeNodePairVec {
+            vec: Vec::new()
+        }
+    }
+    fn push(&mut self, pair: InodeNodePair<'a>) {
+        self.vec.push(pair);
+    }
+    fn node(&self, inode: u64) -> Option<nx::Node<'a>> {
+        match self.vec.iter().find(|p| p.inode == inode) {
+            Some(pair) => Some(pair.node),
+            None => None
+        }
+    }
+    fn inode(&self, node: nx::Node) -> Option<u64> {
+        match self.vec.iter().find(|p| p.node == node) {
+            Some(pair) => Some(pair.inode),
+            None => None
+        }
     }
 }
 
-impl Filesystem for NxFilesystem {
+impl<'a> NxFilesystem<'a> {
+    pub fn new_with_nx_file(nx_file: &'a nx::File) -> NxFilesystem {
+        let pairs = InodeNodePairVec::new();
+        let mut fs = NxFilesystem {
+            nx_file: nx_file,
+            inode_node_pairs: pairs,
+            inode_counter: FUSE_ROOT_ID + 1
+        };
+        // Add root node
+        fs.inode_node_pairs.push(InodeNodePair{ inode: FUSE_ROOT_ID, node: fs.nx_file.root() });
+        fs
+    }
+    fn new_inode(&mut self) -> u64 {
+        let inode = self.inode_counter;
+        self.inode_counter += 1;
+        inode
+    }
+}
+
+fn node_has_children(node: nx::Node) -> bool {
+    node.iter().count() > 0
+}
+
+impl<'a> Filesystem for NxFilesystem<'a> {
+    fn readdir(&mut self, _req: &Request, _ino: u64, _fh: u64, offset: u64,
+               mut reply: ReplyDirectory) {
+        println!("[readdir] ino: {}, offset: {}", offset, _ino);
+        // Ignore inode 0
+        if _ino == 0 {
+            reply.error(ENOENT);
+            return;
+        }
+        // For some reason we assert here that we are at offset 0
+        if offset == 0 {
+            let node_to_read = self.inode_node_pairs.node(_ino)
+                               .expect("Trying to read nonexistent dir");
+            for (i, child) in node_to_read.iter().enumerate() {
+                let file_type = match node_has_children(child) {
+                    true => FileType::Directory,
+                    false => FileType::RegularFile
+                };
+                let inode = match self.inode_node_pairs.inode(child) {
+                    Some(inode) => inode,
+                    // Doesn't have an inode yet, generate one, and insert it to pairs
+                    None => {
+                        let inode = self.new_inode();
+                        self.inode_node_pairs.push(InodeNodePair{ inode: inode, node: child });
+                        inode
+                    }
+                };
+                reply.add(inode, i as u64, file_type, child.name());
+            }
+        }
+        reply.ok();
+    }
 }
